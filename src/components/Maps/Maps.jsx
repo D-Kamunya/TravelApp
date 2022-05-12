5a3e31efa65edfe160caa8960069ee152594593b
kilewa <kilewageorge230@gmail.com>
2022-05-12 20:50:34 +0300

    Google map intergrated

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+import useStyles from './Styles.js';
 
 export const Maps = () => {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px');
+
+  const coordinates = {lat:0, lng:0};
   return (
-    <div>Maps</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact 
+          bootstrapURLKeys={{key:"AIzaSyDAAdexoBV1yCiTlyubVyhlZ-ssQZiPJtA"}}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50,50,50,50]}
+          options={''}
+          onChange={''}
+          onChildClick={''}
+
+        >
+
+      </GoogleMapReact>
+    </div>
   )
 }
